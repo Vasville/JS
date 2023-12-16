5a4c8b3e948c293d1ffdb6c5d729a8eaf83d0da0
Vasiliy Apostolov <147920458+Vasville@users.noreply.github.com>
2023-12-16 19:26:22 +0200

    sp3

--- separateArrayElements.js
+++ separateArrayElements.js
@@ -1,6 +1,7 @@
 // TASK: Given the array: numbersArr bellow, write the code (using a for loop) which will create two new arrays:
 let arr = [1, 6, -3, 5, 2, -8, 0, 4, 3];
 arrevenNumsoddNums = (arr) => {
+    let newArr = []
     let i = 0;
     let evenNums = [i], oddNums = [i];
     for(let i = 0;i<arr.length;i++){
