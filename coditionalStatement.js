74c620b425ed91bbf50a9fcb61bdc5dfde1dde0f
Vasiliy Apostolov <147920458+Vasville@users.noreply.github.com>
2024-01-04 20:53:57 +0200

    cS2

--- coditionalStatement.js
+++ coditionalStatement.js
@@ -1,5 +1,15 @@
 function greetings() {
     var userTown =
     document.getElementByid("town").value;
-}
+
 console.log(userTown)
+if (town= София) {
+    return "Здравей";
+}
+else if (town = Париж) {
+    return "solut";
+}
+else if (town = Лондон) {
+    return "Hi";
+}
+}
